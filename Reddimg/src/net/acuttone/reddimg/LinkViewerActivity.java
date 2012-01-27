95906a268f198a3ba59699fbe47fd282983ac997
Andrea <andreacuttone@gmail.com>
2012-01-27 09:34:58 +0100

    navigation arrows

--- Reddimg/src/net/acuttone/reddimg/LinkViewerActivity.java
+++ Reddimg/src/net/acuttone/reddimg/LinkViewerActivity.java
@@ -1,10 +1,14 @@
 package net.acuttone.reddimg;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -17,7 +21,9 @@ public class LinkViewerActivity extends Activity {
 
 	public static final String LINK_INDEX = "LINK_INDEX";
 	private int currentLinkIndex;
-	private ImageView view;
+	private ImageView viewBitmap;
+	private ImageView viewLeftArrow;
+	private ImageView viewRightArrow;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,16 +37,42 @@ public class LinkViewerActivity extends Activity {
 		TextView titleTextView = (TextView) findViewById(R.id.textViewTitle);
 		titleTextView.setText(redditLink.getTitle());
 		Bitmap bitmap = ReddimgApp.instance().getImageCache().getImage(redditLink.getUrl());
-		view = (ImageView) findViewById(R.id.scrollViewLink).findViewById(R.id.imageViewLink);
-		view.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-		view.setAdjustViewBounds(true);
-		view.setImageBitmap(bitmap);
+		viewBitmap = (ImageView) findViewById(R.id.scrollViewLink).findViewById(R.id.imageViewLink);
+		viewBitmap.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+		viewBitmap.setAdjustViewBounds(true);
+		viewBitmap.setImageBitmap(bitmap);
+		
+		viewLeftArrow = (ImageView) findViewById(R.id.imageleftarrow);
+		viewRightArrow = (ImageView) findViewById(R.id.imagerightarrow);
+		
+		AsyncTask<Void,Integer,Void> fadeTask = new AsyncTask<Void, Integer, Void>() {
+
+			@Override
+			protected Void doInBackground(Void... params) {
+				for(int alpha = 255; alpha >= 0; alpha -= 5) {
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) { }
+					publishProgress(alpha);
+				}
+				return null;
+			}
+			
+			@Override
+			protected void onProgressUpdate(Integer... values) {
+				viewLeftArrow.setAlpha(values[0]);
+				viewRightArrow.setAlpha(values[0]);
+				super.onProgressUpdate(values);
+			}
+			
+		};
+		fadeTask.execute(null);
 	}
 	
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		Drawable drawable = view.getDrawable();
+		Drawable drawable = viewBitmap.getDrawable();
 		if (drawable instanceof BitmapDrawable) {
 		    BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
 		    Bitmap bitmap = bitmapDrawable.getBitmap();
