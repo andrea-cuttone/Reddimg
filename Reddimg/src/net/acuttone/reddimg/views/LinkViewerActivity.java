9a5bd31c5b6688f70ca776d92ed3a768d0f92907
Andrea Cuttone <andreacuttone@gmail.com>
2012-04-10 16:00:16 +0200

    fixed manifest

--- Reddimg/src/net/acuttone/reddimg/views/LinkViewerActivity.java
+++ Reddimg/src/net/acuttone/reddimg/views/LinkViewerActivity.java
@@ -88,8 +88,7 @@ public class LinkViewerActivity extends Activity {
 				if(fadeTask != null) {
 					fadeTask.cancel(true);
 				}
-				textviewLoading.setText("Loading links from "
-						+ ReddimgApp.instance().getLinksQueue().getCurrentSubreddit());
+				textviewLoading.setText("Loading links...");
 				viewLeftArrow.setAlpha(0);
 				viewRightArrow.setAlpha(0);
 				recycleBitmap();
